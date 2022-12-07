b40e938701702446fa3cb16545209cc89aa67e94
Rob Harper <rob.harper@bbc.co.uk>
2022-12-07 16:15:54 +0000

    feat: day 6 part 2

--- src/day6/index.ts
+++ src/day6/index.ts
@@ -2,5 +2,5 @@ import readInput from "../utils/read-input";
 import { findMarker } from "./comms";
 
 readInput('./input/day6.txt')
-  .then(findMarker)
+  .then((data) => [findMarker(data, 4), findMarker(data, 14)])
   .then(console.log)
--- test/day6.test.ts
+++ test/day6.test.ts
@@ -9,3 +9,13 @@ describe('Day 6 Part 1', () => {
     expect(findMarker('zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw', 4)).toBe(11);
   })
 })
+
+describe('Day 6 Part 2', () => {
+  it('Find the position of the first marker', () => {
+    expect(findMarker('mjqjpqmgbljsphdztnvjfqwrcgsmlb', 14)).toBe(19);
+    expect(findMarker('bvwbjplbgvbhsrlpgdmjqwftvncz', 14)).toBe(23);
+    expect(findMarker('nppdvjthqldpwncqszvftbrmjlhg', 14)).toBe(23);
+    expect(findMarker('nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg', 14)).toBe(29);
+    expect(findMarker('zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw', 14)).toBe(26);
+  })
+})
