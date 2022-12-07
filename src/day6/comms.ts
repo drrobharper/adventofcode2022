d1f55bfec8e5c78228f91df5467f0ce0f955610a
Rob Harper <rob.harper@bbc.co.uk>
2022-12-07 08:43:35 +0000

    refactor: day 6 part 1

--- src/day6/comms.ts
+++ src/day6/comms.ts
@@ -1,13 +1,16 @@
-export const findMarker = (datastream: string) : number => {
+export const findMarker = (datastream: string, markerSize: number) : number => {
   const datastreamArr = datastream.split('');
   let markerIdx = 0;
-  for (let i = 3; i < datastreamArr.length; i ++) {
-    if (datastreamArr[i-3] !== datastreamArr[i-2] && datastreamArr[i-3] !== datastreamArr[i-1] && datastreamArr[i-3] !== datastreamArr[i] &&
-      datastreamArr[i-2] !== datastreamArr[i-1] && datastreamArr[i-2] !== datastreamArr[i] &&
-      datastreamArr[i-1] !== datastreamArr[i]) {
-        markerIdx = i;
-        break;
-      }
+  for (let i = markerSize - 1; i < datastreamArr.length; i ++) {
+    const chars = [datastreamArr[i - (markerSize - 1)]];
+    for (let j = markerSize - 2; j >= 0 ; j--) {
+      if (chars.indexOf(datastreamArr[i-j]) >= 0) continue;
+      chars.push(datastreamArr[i-j]);
+    }
+    if (chars.length === markerSize) {
+      markerIdx = i;
+      break;
+    }
   }
   return markerIdx + 1;
 }
--- test/day6.test.ts
+++ test/day6.test.ts
@@ -2,10 +2,10 @@ import { findMarker } from "../src/day6/comms";
 
 describe('Day 6 Part 1', () => {
   it('Find the position of the first marker', () => {
-    expect(findMarker('mjqjpqmgbljsphdztnvjfqwrcgsmlb')).toBe(7);
-    expect(findMarker('bvwbjplbgvbhsrlpgdmjqwftvncz')).toBe(5);
-    expect(findMarker('nppdvjthqldpwncqszvftbrmjlhg')).toBe(6);
-    expect(findMarker('nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg')).toBe(10);
-    expect(findMarker('zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw')).toBe(11);
+    expect(findMarker('mjqjpqmgbljsphdztnvjfqwrcgsmlb', 4)).toBe(7);
+    expect(findMarker('bvwbjplbgvbhsrlpgdmjqwftvncz', 4)).toBe(5);
+    expect(findMarker('nppdvjthqldpwncqszvftbrmjlhg', 4)).toBe(6);
+    expect(findMarker('nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg', 4)).toBe(10);
+    expect(findMarker('zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw', 4)).toBe(11);
   })
 })
